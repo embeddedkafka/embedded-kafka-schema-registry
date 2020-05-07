95bb77373126b2aeb469c299daa7f7040704ae5c
Francesco Pellegrini <francesco.pellegrini@bitrock.it>
2020-05-07 08:12:37 +0200

    Feature/bump scalafmt (#127)
    * bump scalafmt-core version
    
    * add scalafmtCheckAll step to build workflow

--- src/test/scala/net/manub/embeddedkafka/schemaregistry/TestAvroClass.scala
+++ src/test/scala/net/manub/embeddedkafka/schemaregistry/TestAvroClass.scala
@@ -6,19 +6,21 @@ import org.apache.avro.{AvroRuntimeException, Schema}
 case class TestAvroClass(var name: String) extends SpecificRecordBase {
   def this() = this("")
 
-  override def get(i: Int): AnyRef = i match {
-    case 0 => name
-    case _ => throw new AvroRuntimeException("Bad index")
-  }
+  override def get(i: Int): AnyRef =
+    i match {
+      case 0 => name
+      case _ => throw new AvroRuntimeException("Bad index")
+    }
 
-  override def put(i: Int, v: scala.Any): Unit = i match {
-    case 0 =>
-      name = v match {
-        case (utf8: org.apache.avro.util.Utf8) => utf8.toString
-        case _                                 => v.asInstanceOf[String]
-      }
-    case _ => throw new AvroRuntimeException("Bad index")
-  }
+  override def put(i: Int, v: scala.Any): Unit =
+    i match {
+      case 0 =>
+        name = v match {
+          case (utf8: org.apache.avro.util.Utf8) => utf8.toString
+          case _                                 => v.asInstanceOf[String]
+        }
+      case _ => throw new AvroRuntimeException("Bad index")
+    }
 
   override def getSchema: Schema = TestAvroClass.SCHEMA$
 }
