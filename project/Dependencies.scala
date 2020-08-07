e1219841fc3052c1a710d16b4a7e007e9480f93a
Scala Steward <me@scala-steward.org>
2020-08-07 06:43:55 +0200

    Update scalatest-shouldmatchers, ... to 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val EmbeddedKafka     = "2.5.0"
     val ConfluentPlatform = "5.5.1"
     val Slf4j             = "1.7.30"
-    val ScalaTest         = "3.2.0"
+    val ScalaTest         = "3.2.1"
   }
 
   object Common {
