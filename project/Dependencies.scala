505b870b106f4bf21d2ad5d303d6471e1f86b23f
Francesco Pellegrini <francesco.pellegrini@bitrock.it>
2021-02-09 08:37:27 +0100

    bump confluent platform dependency to 6.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val Scala             = "2.13.4"
     val Scala212          = "2.12.12"
     val EmbeddedKafka     = "2.7.0"
-    val ConfluentPlatform = "6.0.1"
+    val ConfluentPlatform = "6.1.0"
     val Slf4j             = "1.7.30"
     val ScalaTest         = "3.2.3"
   }
