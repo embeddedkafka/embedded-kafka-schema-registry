4f696872bbea3408337c9fc2938ae571b5f7084a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-06-08 22:45:20 +0200

    Update kafka-avro-serializer, ... to 6.2.0 (#230)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val Scala             = "2.13.4"
     val Scala212          = "2.12.12"
     val EmbeddedKafka     = "2.7.0"
-    val ConfluentPlatform = "6.1.1"
+    val ConfluentPlatform = "6.2.0"
     val Slf4j             = "1.7.30"
     val ScalaTest         = "3.2.9"
   }
