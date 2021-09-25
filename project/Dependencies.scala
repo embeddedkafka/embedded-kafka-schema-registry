ef6346cb59954ce4b0851fca136bfa8cab02068b
Scala Steward <me@scala-steward.org>
2021-09-25 02:23:54 +0200

    Update embedded-kafka, ... to 2.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   object Versions {
     val Scala             = "2.13.6"
     val Scala212          = "2.12.15"
-    val EmbeddedKafka     = "2.8.0"
+    val EmbeddedKafka     = "2.8.1"
     val ConfluentPlatform = "6.2.1"
     val Slf4j             = "1.7.32"
     val ScalaTest         = "3.2.10"
