3873114e928b0f49035a1312580030b2bcfb43db
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-01-08 11:21:10 +0100

    Update scalatest-shouldmatchers, ... to 3.2.15 (#370)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Francesco Pellegrini <francesco.pelle@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val EmbeddedKafka     = "3.3.1"
     val ConfluentPlatform = "7.3.1"
     val Slf4j             = "1.7.36"
-    val ScalaTest         = "3.2.14"
+    val ScalaTest         = "3.2.15"
   }
 
   object Common {
