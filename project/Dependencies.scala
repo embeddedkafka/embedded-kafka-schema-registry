4652e3c6bdac8016d04b0d45e8e6552dcb1798a4
Scala Steward <scala_steward@virtuslab.com>
2023-04-19 14:56:14 +0000

    Update embedded-kafka, ... to 3.4.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   object Versions {
     val Scala             = "2.13.10"
     val Scala212          = "2.12.17"
-    val EmbeddedKafka     = "3.4.0"
+    val EmbeddedKafka     = "3.4.0.1"
     val ConfluentPlatform = "7.3.3"
     val Slf4j             = "1.7.36"
     val ScalaTest         = "3.2.15"
