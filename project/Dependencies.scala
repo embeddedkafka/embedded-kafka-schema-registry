a9754cdbc086a247f6b0428a2c016e44ae21d5fd
Francesco Pellegrini <francesco.pelle@gmail.com>
2023-05-05 19:42:16 +0200

    cross-compile project for Scala 3 (#388)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,8 @@ object Dependencies {
   }
 
   object Versions {
-    val Scala             = "2.13.10"
+    val Scala3            = "3.2.2"
+    val Scala213          = "2.13.10"
     val Scala212          = "2.12.17"
     val EmbeddedKafka     = "3.4.0.1"
     val ConfluentPlatform = "7.4.0"
