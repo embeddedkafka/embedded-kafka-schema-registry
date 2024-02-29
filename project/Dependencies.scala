bb0648f04da24346820dc40d4361a9bfd294ee86
Scala Steward <scala_steward@virtuslab.com>
2024-02-29 16:30:49 +0000

    Update scala-library to 2.13.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
 
   object Versions {
     val Scala3            = "3.3.1"
-    val Scala213          = "2.13.12"
+    val Scala213          = "2.13.13"
     val Scala212          = "2.12.19"
     val EmbeddedKafka     = "3.6.1"
     val ConfluentPlatform = "7.6.0"
