8428102de78084a1c07ce511d82958c8e11c7993
Francesco Pellegrini <francesco.pellegrini@prima.it>
2024-08-16 14:40:37 +0200

    bump embedded-kafka to v3.7.0 and confluent to 7.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,8 +14,8 @@ object Dependencies {
     val Scala3            = "3.3.3"
     val Scala213          = "2.13.14"
     val Scala212          = "2.12.19"
-    val EmbeddedKafka     = "3.6.1"
-    val ConfluentPlatform = "7.6.2"
+    val EmbeddedKafka     = "3.7.0"
+    val ConfluentPlatform = "7.7.0"
     val Slf4j             = "1.7.36"
     val ScalaTest         = "3.2.19"
   }
