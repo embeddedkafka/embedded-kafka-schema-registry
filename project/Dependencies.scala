e80a1a2c9a1a824e9704fe8f00bcbb4b9f1d0387
Scala Steward <scala_steward@virtuslab.com>
2025-09-29 14:06:38 +0000

    Update kafka-avro-serializer, ... to 8.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val Scala3            = "3.3.6"
     val Scala213          = "2.13.16"
     val EmbeddedKafka     = "4.0.1.1"
-    val ConfluentPlatform = "8.0.0"
+    val ConfluentPlatform = "8.0.1"
     val Slf4j             = "2.0.17"
     val ScalaTest         = "3.2.19"
   }
